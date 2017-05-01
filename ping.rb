b58ba0965fe7a153cf6e22bdf4b96a938a2c8161
JohnsonYang0972 <asio19810627@gmail.com>
2017-05-01 10:46:30 +0800

    add ping string cmp

--- ping.rb
+++ ping.rb
@@ -24,15 +24,22 @@ def my_ping(ip_address)
 	#timeout_period = 10
         # Timeout.timeout(10) do
             result = `ping -n 2 "#{ip_address}"`
-	    puts result
+	    #puts result
+	    if result =~ /Packets: +Sent = 2, Received = 2, Lost = 0 / then
+		 puts "my_ping #{ip_address} OK"
+	end
          #end
   end
  def my_ping2()
 	 result = `ping -n 2 127.0.0.1`
-	 puts result
+	 #puts result
+	 if result =~ /Packets: +Sent = 2, Received = 2, Lost = 0 / then
+		 puts "my_ping2 127.0.0.1 OK"
+	end
  end
 
-my_ping('127.0.0.1')
-my_ping2
 puts check_ping_ok('127.0.0.1')
 puts check_ping_ok('192.168.1.1')
+my_ping2
+my_ping('127.0.0.1')
+my_ping('192.168.1.1')
